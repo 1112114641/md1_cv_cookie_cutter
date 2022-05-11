fd81dd6dbd5cde0059d66f21b35b12c84ba3b6d7
md1 <markus.dollmann@eliiza.com.au>
2022-05-11 11:24:20 +1000

    wip(data_loader & augmentations)

--- src/data_loader.py
+++ src/data_loader.py
@@ -9,11 +9,13 @@ dl = DataLoader()
 ```
 """
 import os
+import numpy as np
 import logging
 import pandas as pd
 import tensorflow as tf
 from typing import Tuple
 from abstracts import DataLoaderABC
+from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
 
 # import joblib
 # import numpy as np
@@ -29,7 +31,7 @@ class DataLoader(DataLoaderABC):
         super().__init__(config)
         self.labels = self.read_labels(self.config.labels["label_dir"])
 
-    def data_generator(self) -> Tuple[tf.data.Dataset]:
+    def data_generator(self, *args) -> Tuple[tf.data.Dataset]:
         """Create data ready for training."""
         # image_filenames = [
         # os.path.join(dp, f) for dp, dn, filenames in os.walk(path) for f in filenames
@@ -51,14 +53,14 @@ class DataLoader(DataLoaderABC):
             # image = tf.image.rgb_to_grayscale(image)
             img_size = self.config.data["img_height"]
             image = tf.image.resize(image, size=[img_size, img_size])
-            image = tf.image.convert_image_dtype(image, tf.uint8)
+            image = tf.image.convert_image_dtype(image, tf.float32) / 255.0
             # image = tf.py_function(func=augment_images, inp=[image], Tout=tf.uint8)
             return image
 
         images_ds = filenames_ds.map(
             _parse_image, num_parallel_calls=tf.data.experimental.AUTOTUNE
         )
-        # FIXME: check one-hot requirement for multi label?
+        # FIXME: check one-hot requirement for multi label? Object Detection?
         labels_ds = tf.data.Dataset.from_tensor_slices(self.labels.labels)
         ds = tf.data.Dataset.zip((images_ds, labels_ds))
 
@@ -87,6 +89,60 @@ class DataLoader(DataLoaderABC):
         ds_train, ds_valid, ds_test = configure_for_performance(ds)
         return ds_train, ds_valid, ds_test
 
+    def _split(
+        self,
+    ) -> Tuple[pd.DataFrame]:
+        """
+        Returns indices of stratified train/test split set.
+        If `self.config.labels["val_test_subsplits"]`, ensure each item occurs with a
+        sufficient frequency.
+        Caution: does not filter for duplicate files in the file list!
+
+        c.f. https://github.com/trent-b/iterative-stratification
+        # FIXME: multi-label input (vis a vis self.read_labels())
+        # FIXME: if labels elements are not list-like, extend dim to list like for stratifier
+        """
+        mlsss_train_valtest = MultilabelStratifiedShuffleSplit(
+            n_splits=2,
+            train_size=(1 - self.config.labels["train_test_split"]),
+            test_size=self.config.labels["train_test_split"],
+            random_state=self.config.labels["random_seed"],
+        )
+
+        Y = self.labels.labels  # labels to be stratified,
+        try:
+            test = self.labels.labels[0] * 1
+        except IndexError:
+            # FIXME
+            test = 2
+            test = test
+
+        X = np.zeros(self.labels.shape[0])  # placeholder
+
+        train_indices, valid_indices = next(mlsss_train_valtest.split(X, Y))
+        self.train_labels = self.labels[train_indices]
+        self.valid_labels = self.labels[valid_indices]
+
+        if self.config.labels["val_test_subsplits"] > 0:
+            mlsss_val_test = MultilabelStratifiedShuffleSplit(
+                n_splits=2,
+                train_size=(1 - self.config.labels["val_test_subsplits"]),
+                test_size=self.config.labels["val_test_subsplits"],
+                random_state=self.config.labels["random_seed"],
+            )
+            valid_indices, test_indices = next(
+                mlsss_val_test.split(
+                    np.zeros(self.valid_labels.shape[0]), self.valid_labels.labels
+                )
+            )
+            self.valid_labels = self.labels[valid_indices]
+            self.test_labels = self.labels[test_indices]
+        return (
+            self.train_labels,
+            self.valid_labels,
+            self.test_labels,
+        )
+
     def persist_data_set(self) -> None:
         """
         Create augmented data given a certain random seed, and save to
@@ -104,7 +160,20 @@ class DataLoader(DataLoaderABC):
         pass
 
     def read_labels(self, label_dir: str = None) -> pd.DataFrame:
-        """Read labels, split into one hot labels for multi label case."""
+        """
+        Read labels, split into one hot labels for multi label case.
+        For classification task, labels are expected to come in the column format:
+         filename, label1, label2, label3, ...
+        For object detection the format should be:
+         filename, label1, h1, w1, x1, y1, label2, h2, w2, x2, y2, ...
+
+        # FIXME: object detection bounding boxes
+
+          Returns:
+            pd.DataFrame: filenames, labels (and bounding boxes) in the forma split up
+            into columns into columns like:
+            fname:str, labels:List[str], bbox1_hwxy:List[int], bbox2_hwxy:List[int], ...
+        """
         label_dir = label_dir if label_dir else self.label_dir
 
         if os.path.isile(label_dir) & label_dir.endswith(".csv"):
