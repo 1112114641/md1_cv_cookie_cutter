79775dedcc627ae57094870f966b19ab1b8ece74
md1 <markus.dollmann@eliiza.com.au>
2022-05-12 17:01:04 +1000

    wip(data_loader.generator augment logic)

--- src/data_loader.py
+++ src/data_loader.py
@@ -42,54 +42,52 @@ class DataLoader(DataLoaderABC):
         image_filenames = self.labels.filename
         filenames_ds = tf.data.Dataset.from_tensor_slices(image_filenames)
 
-        def _parse_image(filename: str) -> tf.Tensor:
-            """
-            Read in image data. Normalisation to be performed by the model at train/
-            inference run-time.
-            """
-            try:
-                image = tf.io.read_file(self.config.data["data_dir"] + filename)
-            except FileNotFoundError:
-                logging.info(f"{filename} not found in {self.config.data['data_dir']}.")
-            image = tf.io.decode_image(image, channels=3)
-            # image = tf.image.rgb_to_grayscale(image)
-            img_size = self.config.data["img_height"]
-            image = tf.image.resize(image, size=[img_size, img_size])
-            image = tf.image.convert_image_dtype(image, tf.float32) / 255.0
-            # image = tf.py_function(func=augment_images, inp=[image], Tout=tf.uint8)
-            return image
-
         images_ds = filenames_ds.map(
-            _parse_image, num_parallel_calls=tf.data.experimental.AUTOTUNE
+            self._parse_image, num_parallel_calls=tf.data.experimental.AUTOTUNE
         )
-        # FIXME: check one-hot requirement for multi label? Object Detection?
+
         labels_ds = tf.data.Dataset.from_tensor_slices(self.labels.labels)
         ds = tf.data.Dataset.zip((images_ds, labels_ds))
 
-        def configure_for_performance(ds) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
-            """
-            Create train/val/test split, augment train, and optimise for fast data
-            loading.
-            """
-            val_split = self.config.labels["train_test_split"]
-            test_split = self.config.labels["val_test_subsplits"]
-            no_of_images = self.labels.shape[0]
-            ds_valid = ds.take(int(val_split * no_of_images))
-            ds_valid = ds_valid.batch(BATCH_SIZE)
-            ds_valid = ds_valid.prefetch(buffer_size=AUTOTUNE)
-            ds_test = ds.skip(int(val_split * no_of_images))
-            ds_test = ds_test.take(int(test_split * no_of_images))
-            ds_test = ds_test.batch(BATCH_SIZE)
-            ds_train = ds.skip(
-                int(val_split * no_of_images) + int(test_split * no_of_images)
-            )
-            ds_train = ds_train.shuffle(buffer_size=10000)
-            ds_train = ds_train.batch(BATCH_SIZE).repeat()
-            ds_train = ds_train.prefetch(buffer_size=AUTOTUNE)
-            return ds_train, ds_valid, ds_test
+        # prep train/val/test split
+        val_split = self.config.labels["train_test_split"]
+        test_split = self.config.labels["val_test_subsplits"]
+        no_of_images = self.labels.shape[0]
 
-        ds_train, ds_valid, ds_test = configure_for_performance(ds)
+        # split data
+        ds_valid = ds.take(int(val_split * no_of_images))
+        ds_valid = ds_valid.batch(BATCH_SIZE)
+        ds_valid = ds_valid.prefetch(buffer_size=AUTOTUNE)
+        ds_test = ds.skip(int(val_split * no_of_images))
+        ds_test = ds_test.take(int(test_split * no_of_images))
+        ds_test = ds_test.batch(BATCH_SIZE)
+        ds_train = ds.skip(
+            int(val_split * no_of_images) + int(test_split * no_of_images)
+        )
+
+        # create augmented train data
+        def _augmented_train_data(ds: tf.data.Dataset) -> tf.data.Dataset:
+            # FIXME: fill in logic here
+            return ds
+
+        ds_train, ds_valid, ds_test = self._configure_for_performance(ds)
         return ds_train, ds_valid, ds_test
+
+    def persist_data_set(self) -> None:
+        """
+        Create augmented data given a certain random seed, and save to
+        `DataLoader.config.data.augment_dir`.
+        """
+        loader = self.data_generator()
+        loader = loader
+        amount_to_create = (
+            self.labels.shape[0] * self.config["augment_tf"]["augmentations_amount"]
+        )
+        for _ in range(amount_to_create):
+            a = 1
+            a = a
+            # create images
+        pass
 
     def _train_test_split_labels(
         self,
@@ -154,6 +152,8 @@ class DataLoader(DataLoaderABC):
                 )
                 self.valid_labels = self.labels[y_valid.index]
                 self.test_labels = self.labels[y_test.index]
+            else:
+                self.test_labels = pd.DataFrame(columns=["filename"])
 
         return (
             self.train_labels,
@@ -161,21 +161,33 @@ class DataLoader(DataLoaderABC):
             self.test_labels,
         )
 
-    def persist_data_set(self) -> None:
+    def _parse_image(self, filename: str) -> tf.Tensor:
         """
-        Create augmented data given a certain random seed, and save to
-        `DataLoader.config.data.augment_dir`.
+        Read in image data. Normalisation to be performed by the model at train/
+        inference run-time.
         """
-        loader = self.data_generator()
-        loader = loader
-        amount_to_create = (
-            self.labels.shape[0] * self.config["augment_tf"]["augmentations_amount"]
-        )
-        for _ in range(amount_to_create):
-            a = 1
-            a = a
-            # create images
-        pass
+        try:
+            image = tf.io.read_file(self.config.data["data_dir"] + filename)
+        except FileNotFoundError:
+            logging.info(f"{filename} not found in {self.config.data['data_dir']}.")
+        image = tf.io.decode_image(image, channels=3)
+        # image = tf.image.rgb_to_grayscale(image)
+        img_size = self.config.data["img_height"]
+        image = tf.image.resize(image, size=[img_size, img_size])
+        image = tf.image.convert_image_dtype(image, tf.float32) / 255.0
+        # image = tf.py_function(func=augment_images, inp=[image], Tout=tf.uint8)
+        return image
+
+    def _configure_for_performance(self, ds: tf.data.Dataset) -> tf.data.Dataset:
+        """
+        Create train/val/test split, augment train, and optimise for fast data
+        loading.
+        FIXME: remove datasplitting and add it explicitly into data_loader
+        """
+        ds = ds.shuffle(buffer_size=10000)
+        ds = ds.batch(BATCH_SIZE).repeat()
+        ds = ds.prefetch(buffer_size=AUTOTUNE)
+        return ds
 
     def read_labels(self, label_dir: str = None) -> pd.DataFrame:
         """
@@ -282,13 +294,13 @@ class DataLoader(DataLoaderABC):
                 )
             )
 
-        # for the case of multi-label data:
-        if isinstance(labels_df["labels"].iloc[0], list):
-            (
-                labels_df.drop("labels", axis=1).join(
-                    labels_df.labels.str.join("|").str.get_dummies()
-                )
-            )
+        # # for the case of multi-label data:
+        # if isinstance(labels_df["labels"].iloc[0], list):
+        #   (
+        #     labels_df.drop("labels", axis=1).join(
+        #       labels_df.labels.str.join("|").str.get_dummies()
+        #     )
+        #   )
         return labels_df
 
 
